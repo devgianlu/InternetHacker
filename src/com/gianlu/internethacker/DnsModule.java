f07c58698c6564f43e9b9081cb6c870f54e62f0b
Gianlu <altomanigianluca@gmail.com>
2018-08-22 17:01:19 +0200

    Avoid ID conflicts by generating an internal ID

--- src/com/gianlu/internethacker/DnsModule.java
+++ src/com/gianlu/internethacker/DnsModule.java
@@ -1,5 +1,6 @@
 package com.gianlu.internethacker;
 
+import com.gianlu.internethacker.models.DnsHeader;
 import com.gianlu.internethacker.models.DnsMessage;
 
 import java.io.ByteArrayOutputStream;
@@ -10,12 +11,15 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DnsModule implements Closeable {
     private static final Logger logger = Logger.getLogger(DnsModule.class.getName());
     private static final SocketAddress DEFAULT_SERVER;
+    private static final long MAX_TIMEOUT = TimeUnit.SECONDS.toMillis(5);
 
     static {
         try {
@@ -27,16 +31,47 @@ public class DnsModule implements Closeable {
 
     private final Runner runner;
     private final ExecutorService executorService = Executors.newCachedThreadPool();
-    private final Map<Short, SocketAddress> idToRecipient = new ConcurrentHashMap<>();
+    private final Map<Short, RecipientHolder> ownIdToRecipient = new ConcurrentHashMap<>();
+    private final AtomicReference<Short> ownIdCounter = new AtomicReference<>(Short.MIN_VALUE);
 
     public DnsModule(int port) throws IOException {
         this.runner = new Runner(port);
         new Thread(runner).start();
+
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() ->
+                        ownIdToRecipient.values().removeIf(recipientHolder ->
+                                System.currentTimeMillis() - recipientHolder.timestamp > MAX_TIMEOUT),
+                0, MAX_TIMEOUT, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void close() {
         runner.close();
+    }
+
+    public static class RecipientHolder {
+        private final SocketAddress address;
+        private final short originalId;
+        private final long timestamp;
+
+        public RecipientHolder(DnsHeader header, DatagramPacket packet) {
+            this.originalId = header.id;
+            this.address = packet.getSocketAddress();
+            this.timestamp = System.currentTimeMillis();
+        }
+
+        public void sendBack(DnsMessage message, DatagramSocket socket) throws IOException {
+            message = message.buildUpon()
+                    .setHeader(message.header.buildUpon()
+                            .setId(originalId)
+                            .build())
+                    .build();
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            message.write(out);
+            DatagramPacket packet = new DatagramPacket(out.toByteArray(), out.size(), address);
+            socket.send(packet);
+        }
     }
 
     private class ServingClient implements Runnable {
@@ -51,28 +86,35 @@ public class DnsModule implements Closeable {
         @Override
         public void run() {
             DnsMessage message = new DnsMessage(packet.getData());
-            SocketAddress recipient = idToRecipient.remove(message.header.id);
-
-            ByteArrayOutputStream out;
-            try {
-                out = new ByteArrayOutputStream();
-                message.write(out);
-            } catch (IOException ex) {
-                logger.log(Level.SEVERE, "Failed writing packet to the stream.", ex);
-                return;
-            }
+            RecipientHolder recipient = ownIdToRecipient.remove(message.header.id);
 
             if (recipient != null) {
                 try {
-                    DatagramPacket packet = new DatagramPacket(out.toByteArray(), out.size(), recipient);
-                    socket.send(packet);
+                    recipient.sendBack(message, socket);
                 } catch (IOException ex) {
                     logger.log(Level.SEVERE, "Failed sending packet to client.", ex);
                 }
             } else {
-                idToRecipient.put(message.header.id, packet.getSocketAddress());
+                short ownId;
+                synchronized (ownIdCounter) {
+                    ownId = ownIdCounter.getAndUpdate(aShort -> {
+                        if (aShort == Short.MAX_VALUE) return Short.MIN_VALUE;
+                        else return ++aShort;
+                    });
+                }
+
+                recipient = new RecipientHolder(message.header, packet);
+                ownIdToRecipient.put(ownId, recipient);
+
+                message = message.buildUpon()
+                        .setHeader(message.header.buildUpon()
+                                .setId(ownId)
+                                .build())
+                        .build();
 
                 try {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    message.write(out);
                     DatagramPacket packet = new DatagramPacket(out.toByteArray(), out.size(), DEFAULT_SERVER);
                     socket.send(packet);
                 } catch (IOException ex) {
